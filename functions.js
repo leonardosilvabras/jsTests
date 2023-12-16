eaee52e0201943b3a1d84f6bf3527ff3d34ed6d9
Leonardo Silva <103190146+leosilvafx@users.noreply.github.com>
2023-12-16 18:57:48 -0300

    code

--- functions.js
+++ functions.js
@@ -1,5 +1,41 @@
-let createPhrases = () => {
-    console.log('leo')
+let sum = (...args) => {
+  let result = 0;
+  for (const num of args) {
+    result += num;
+  }
+  return result;
+};
+
+class Students {
+  constructor(nome, idade, sexo, anoLetivo, matricula) {
+    this.nome = nome;
+    this.idade = idade;
+    this.sexo = sexo;
+    this.anoLetivo = anoLetivo;
+    this.matricula = matricula;
+  }
 }
 
-createPhrases()
+let students = [
+  (F2401 = new Students(
+    "Leonardo Henrique Gabriel da Silva",
+    20,
+    "M",
+    3,
+    true
+  )),
+  (F0512 = new Students("Beatriz Moniele da Silva", 25, "F", 2, false)),
+];
+
+let mediaAlunos = () => {
+  let totalIdades = 0;
+
+  for (i = 0; i < students.length; i++) {
+    totalIdades += students[i].idade;
+  }
+  let mediaIdade = totalIdades / students.length;
+
+  return mediaIdade;
+};
+
+console.log(mediaAlunos());
