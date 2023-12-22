b7687a82582c4152846ed828a2e0f3eb201df749
Leonardo Silva <103190146+leosilvafx@users.noreply.github.com>
2023-12-22 20:45:24 -0300

    code

--- list.js
+++ list.js
@@ -2,11 +2,11 @@ let shoppingList = [];
 shoppingCartCapacity = 10;
 
 for (let items = 0; items < shoppingCartCapacity; items++) {
-    shoppingList[items] = prompt(
+  shoppingList[items] = prompt(
     "Digite o nome do item que vai ser colocado na sua compra:"
   );
 
-  if (items === (shoppingCartCapacity-1)) {
+  if (items === shoppingCartCapacity - 1) {
     alert(`Seu carrinho atingiu o limite de items.\n
     Os items contidos no seu carrinho sao: ${shoppingList.join(",")}`);
   }
