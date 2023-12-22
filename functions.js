0e1e1f982a82bdfc99bba1cb35e1d67890cd28b6
Leonardo Silva <103190146+leosilvafx@users.noreply.github.com>
2023-12-22 20:21:55 -0300

    code

--- functions.js
+++ functions.js
@@ -1,4 +1,4 @@
-class Students {
+class Student {
   constructor(
     nome,
     idade,
@@ -24,71 +24,86 @@ class Students {
   }
 }
 
-const students = [
-  (F2401 = new Students(
-    "Leonardo Henrique Gabriel da Silva",
-    20,
-    "M",
-    3,
-    true
-  )),
-  (F0512 = new Students("Beatriz Moniele da Silva", 25, "F", 2, false)),
-];
-
-const mediaAlunos = () => {
-  for (i = 0, mediaIdade = 0; i < students.length; i++) {
-    mediaIdade += students[i].idade / students.length;
-  }
-  return mediaIdade;
-};
-
-console.log(mediaAlunos());
-
-let alunoNotas = [];
+let students = [];
+Leonardo = new Student("Leonardo Henrique Gabriel da Silva", 20, "M", 2, true);
+Beatriz = new Student("Beatriz Moniele da Silva", 25, "F", 3, false);
+alunoNotas = [];
 mediaNotas = 0;
 alunoNotas.length = 3;
 mediaEscolar = 6;
+toFixedNumbers = 2;
+
+students.push(Leonardo, Beatriz);
 
 for (let aluno = 0; aluno < students.length; aluno++) {
-  for (let notas = 1, i = 0; notas < alunoNotas.length + 1; notas++, i++) {
-    alunoNotas[i] = Number(prompt(`Digite a nota da prova ${notas}:`));
-    switch (notas) {
-      case 1:
-        students[aluno].n1 = alunoNotas[i];
-        break;
-      case 2:
-        students[aluno].n2 = alunoNotas[i];
-        break;
-      case 3:
-        students[aluno].n3 = alunoNotas[i];
-        break;
-      default:
-        break;
-    }
+  if (students[aluno].matricula === true) {
+    for (let notas = 1, i = 0; notas < alunoNotas.length + 1; notas++, i++) {
+      alunoNotas[i] = Number(prompt(`Digite a nota da prova n${notas}:`));
 
-    mediaNotas += alunoNotas[i] / alunoNotas.length;
-
-    if (i === 2) {
-      students[aluno].media = mediaNotas;
-      mediaNotas = 0;
-
-      if (students[aluno].media < mediaEscolar) {
-        students[aluno].status = "recuperação";
+      if (alunoNotas[i] <= 10) {
+        switch (notas) {
+          case 1:
+            students[aluno].n1 = alunoNotas[i].toFixed(toFixedNumbers);
+            break;
+          case 2:
+            students[aluno].n2 = alunoNotas[i].toFixed(toFixedNumbers);
+            break;
+          case 3:
+            students[aluno].n3 = alunoNotas[i].toFixed(toFixedNumbers);
+            break;
+          default:
+            break;
+        }
       } else {
-        students[aluno].status = "aprovado";
+        console.log("Nota excede o limite estabelecido");
+        break;
+      }
+
+      mediaNotas += alunoNotas[i] / alunoNotas.length;
+
+      if (i === 2) {
+        students[aluno].media = mediaNotas.toFixed(toFixedNumbers);
+        mediaNotas = 0;
+
+        if (students[aluno].media < mediaEscolar) {
+          students[aluno].status = "Recuperação";
+        } else {
+          students[aluno].status = "Aprovado";
+        }
+      }
+      if (students[aluno].status != undefined) {
+        let formResult = `idade: ${students[aluno].idade}\n
+        sexo: ${students[aluno].sexo}\n
+        anoLetivo: ${students[aluno].anoLetivo}\n
+        matricula: ${students[aluno].matricula}\n
+        n1: ${students[aluno].n1}\n
+        n2: ${students[aluno].n2}\n
+        n3: ${students[aluno].n3}\n
+        media ${students[aluno].media}\n
+        status: ${students[aluno].status}\n
+        `;
+
+        if (students[aluno].sexo === "M") {
+          console.log(`Dados finais do aluno ${students[aluno].nome}:\n
+          ${formResult}
+          `);
+          console.log("%cA Escola Técnica Estadual do Estado de São Paulo deseja ao aluno boas festas!", "background: #000; color: #FF0000");
+        } else {
+          console.log(`Dados finais da aluna ${students[aluno].nome}:\n
+          ${formResult}
+          `);
+        }
       }
     }
+  } else {
+    if (students[aluno].sexo === "M") {
+      console.log(
+        `Matricula inativa.\nNão é possível finalizar o ano letivo do aluno ${students[aluno].nome}`
+      );
+    } else {
+      console.log(
+        `Matricula inativa.\nNão é possível finalizar o ano letivo da aluna ${students[aluno].nome}`
+      );
+    }
   }
-
-  console.log(`Dados finais do aluno ${students[aluno].nome}:\n
-  idade: ${students[aluno].idade}\n
-  sexo: ${students[aluno].sexo}\n
-  anoLetivo: ${students[aluno].anoLetivo}\n
-  matricula: ${students[aluno].matricula}\n
-  n1: ${students[aluno].n1}\n
-  n2: ${students[aluno].n2}\n
-  n3:"${students[aluno].n3}\n
-  media ${students[aluno].media}\n
-  status: ${students[aluno].status}\n
-  A Escola Técnica Estadual do Estado de São Paulo deseja ao aluno boas festas!`);
 }
