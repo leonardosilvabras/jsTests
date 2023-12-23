998e7d2ae1e571a51ded335260ec7fb4faf8b6bf
Leonardo Silva <103190146+leosilvafx@users.noreply.github.com>
2023-12-22 22:09:30 -0300

    code

--- randomNumber.js
+++ randomNumber.js
@@ -1,10 +1,10 @@
 let random = Math.floor(Math.random() * 5),
-kick,
-kicks=0;
+  kick,
+  kicks = 0;
 
 while (kick != random) {
-    kick = Number(prompt("Adivinhe o numero sorteado no intervalo de 0 a 4:"));
-    kicks++
+  kick = Number(prompt("Adivinhe o numero sorteado no intervalo de 0 a 4:"));
+  kicks++;
 }
 
-alert(`Número correto.\nVoce realizou ${kicks} tentativas.`)
+alert(`Número correto.\nVoce realizou ${kicks} tentativas.`);
